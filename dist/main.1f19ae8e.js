672d5f41a844809e68f40f254ccb2eebcd40c391
wujiale-spec <1284698788@qq.com>
2020-09-11 14:39:14 +0800

    init

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -181,7 +181,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50682" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51965" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
